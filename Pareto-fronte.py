a71acfbacc256a1596523b1bff04cab3d331d225
njenko <nejc.jenko1@gmail.com>
2022-01-10 10:38:02 +0100

    grafi, popravki komentarjev kode

--- Pareto-fronte.py
+++ Pareto-fronte.py
@@ -5,32 +5,32 @@ from numpy import random, linalg
 #===============================================================================
 
 def dominira(a, b):
-    return sum([a[x] >= b[x] for x in range(len(b))]) == len(b) # vrne true, če točka a dominira točko b
+    return sum([a[x] >= b[x] for x in range(len(b))]) == len(b) # Vrne true, če točka a dominira točko b
 
-def izracun_pareto_fronte(mnozica): #funkcija iz dane množice vrne dve množici: točke 1. pareto fronte in vse dominirane točke
+def izracun_pareto_fronte(mnozica): # Funkcija iz dane množice vrne dve množici: točke 1. pareto fronte in vse dominirane točke
     pareto_fronta = set()
     dominirane_tocke = set()
 
     while len(mnozica) != 0:
-        kandidat = list(mnozica)[0] #vzamemo točko in jo odstranimo iz množice
+        kandidat = list(mnozica)[0] # Vzamemo točko in jo odstranimo iz množice
         mnozica.remove(kandidat)
         i = 0
-        ni_dominirana = True #indikator, ki preverja ali je ta točka dominirana
+        ni_dominirana = True # Indikator, ki preverja ali je ta točka dominirana
 
         while i < len(mnozica):
             tocka = list(mnozica)[i]
-            if dominira(kandidat, tocka): #preverimo, ali naša izbrana točka (kandidat) dominira ostale točke. Če katero izmed njih dominira, tisto točko damo v množico dominiranih točki in jo odstranimo iz osnovne množice, saj je ne rabimo več pregledovati.
+            if dominira(kandidat, tocka): # Preverimo, ali naša izbrana točka (kandidat) dominira ostale točke. Če katero izmed njih dominira, tisto točko damo v množico dominiranih točki in jo odstranimo iz osnovne množice, saj je ne rabimo več pregledovati.
                 mnozica.remove(tocka)
                 dominirane_tocke.add(tuple(tocka))
                 i += 1
-            elif dominira(tocka, kandidat): #če najdemo točko, ki dominira našega kandidata, lahko kandidata damo v množico dominiranih točk in spremenimo vrednost indikatorja.
+            elif dominira(tocka, kandidat): # Če najdemo točko, ki dominira našega kandidata, lahko kandidata damo v množico dominiranih točk in spremenimo vrednost indikatorja.
                 dominirane_tocke.add(tuple(kandidat))
                 ni_dominirana = False
                 break
             else:
                 i += 1
 
-        if ni_dominirana:   #če ne najdemo točke ki bi dominirala našega kandidata pomeni, da je le ta v skyline-u (pareto fronta)
+        if ni_dominirana:   # Če ne najdemo točke ki bi dominirala našega kandidata pomeni, da je le ta v skyline-u (pareto fronta)
             pareto_fronta.add(tuple(kandidat))
         
         if len(mnozica) == 0:
@@ -55,10 +55,10 @@ def izracun_n_pareto_front(mnozica, n): # 2., 3., 4., ... pareto fronte izračun
 def random_krogla(stevilo_tock, d, radij=1):
     mnozica_tock = set()
     for i in range(stevilo_tock):    
-        rand_smer = random.uniform(low=-1, high=1, size=d) #generiramo nakljucni vektor dimenzije d
-        rand_smer /= linalg.norm(rand_smer, axis=0) #vektor normiramo
-        rand_dolzina = random.uniform(low=0, high=1) # vektor pomnožimo z naključno vrednostjo med 0 in 1, da dobimo točko v notranjosti d dimenzionalne enotske krogle
-        tocka = tuple(radij * (rand_smer * rand_dolzina)) #če bi želeli gledati večjo kroglo lahko spremenimo tudi vrednost radija.
+        rand_smer = random.uniform(low=-1, high=1, size=d) # Generiramo nakljucni vektor dimenzije d
+        rand_smer /= linalg.norm(rand_smer, axis=0) # Vektor normiramo
+        rand_dolzina = random.uniform(low=0, high=1) # Vektor pomnožimo z naključno vrednostjo med 0 in 1, da dobimo točko v notranjosti d dimenzionalne enotske krogle
+        tocka = tuple(radij * (rand_smer * rand_dolzina)) # Če bi želeli gledati večjo kroglo lahko spremenimo tudi vrednost radija.
         mnozica_tock.add(tocka)
     return mnozica_tock
 
@@ -75,24 +75,20 @@ def random_kocka(stevilo_tock, d, a=1): # a stranica kocke, d dimenzija prostora
 # EKSPERIMENTALNI DEL
 #===============================================================================
 
-testne_tocke50_1 = random_krogla(200, 5)
-testne_tocke50_2 = random_krogla(200, 5)
-testne_tocke50_3 = random_krogla(200, 5)
-testne_tocke50_4 = random_krogla(200, 5)
-testne_tocke50_5 = random_krogla(200, 5)
-testne_tocke50_6 = random_krogla(200, 5)
-testne_tocke50_7 = random_krogla(200, 5)
-testne_tocke50_8 = random_krogla(200, 5)
-testne_tocke50_9 = random_krogla(200, 5)
-testne_tocke50_10 = random_krogla(200, 5)
+# Za eno ponovitev eksperimenta najprej generiramo množico podatkov s funkcijo
+# random_krogla ali funkcijo random_kocka, kjer kot parametre vstavimo število 
+# točk ki jih želimo, dimenzijo prostora in pa velikost krogle/kocke. Kot 
+# default je nastavljena enotska krogla in enotska kocka (radij=1 oz. stranica=1)
 
-izracun_n_pareto_front(testne_tocke50_1, 5)
-izracun_n_pareto_front(testne_tocke50_2, 5)
-izracun_n_pareto_front(testne_tocke50_3, 5)
-izracun_n_pareto_front(testne_tocke50_4, 5)
-izracun_n_pareto_front(testne_tocke50_5, 5)
-izracun_n_pareto_front(testne_tocke50_6, 5)
-izracun_n_pareto_front(testne_tocke50_7, 5)
-izracun_n_pareto_front(testne_tocke50_8, 5)
-izracun_n_pareto_front(testne_tocke50_9, 5)
-izracun_n_pareto_front(testne_tocke50_10, 5)
+# Primer:
+testne_tocke = random_krogla(200, 3)
+
+# Nato lahko na tej množici uporabimo funkcijo izracun_pareto_fronte, ki nam vrne 
+# dvojec 1. pareto fronto in pa množice vseh dominiranih točk, ali pa 
+# izracun_n_pareto_front, ki nam vrne vse pareto fronte do globine n oz. do 
+# globine, kjer smo porabili vse točke.
+
+#Primer:
+(pareto, dominirane) = izracun_pareto_fronte(testne_tocke)
+
+pareto_fronte_do_globine_5 = izracun_n_pareto_front(testne_tocke)
